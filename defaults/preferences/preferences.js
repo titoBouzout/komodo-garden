081de9aa580b4464c713dce2d81dff0e8f81f250
Tito Bouzout <tito.bouzout@gmail.com>
2011-05-16 02:21:36 -0300

    add preference "overwrite no ask"

--- defaults/preferences/preferences.js
+++ defaults/preferences/preferences.js
@@ -4,3 +4,5 @@ pref('extensions.asynchremote.log.show.progress', true);
 pref('extensions.asynchremote.log.show.sucess', true);
 
 pref('extensions.asynchremote.upload.and.rename', true);
+pref('extensions.asynchremote.overwrite.no.ask', false);
+
