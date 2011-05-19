9efa2916f1998bc35dee718186b96356d97d9279
Tito Bouzout <tito.bouzout@gmail.com>
2011-05-19 17:15:09 -0300

    Add preferences for saving the dirty document before upload

--- defaults/preferences/preferences.js
+++ defaults/preferences/preferences.js
@@ -6,3 +6,7 @@ pref('extensions.asynchremote.log.show.sucess', true);
 pref('extensions.asynchremote.upload.and.rename', true);
 pref('extensions.asynchremote.overwrite.no.ask', false);
 
+pref('extensions.asynchremote.dont.cache.last.modified', false);
+
+pref('extensions.asynchremote.save.before.upload', false);
+
